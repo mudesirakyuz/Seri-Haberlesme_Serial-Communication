a4ad1ae5953d336b3a98dd08e7f84b7769a70b98
Müddessir <72167899+mudesirakyuz@users.noreply.github.com>
2022-03-21 17:54:52 +0300

    Add files via upload

---
+++ Serial Communication/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Serial_Communication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(471, 28);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Green;
+            this.button2.Enabled = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(12, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(470, 59);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "VERİ AL";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Enabled = false;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(12, 130);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(470, 41);
+            this.button3.TabIndex = 4;
+            this.button3.Text = ">>> DURDUR <<<";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Teal;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(12, 81);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(471, 43);
+            this.button4.TabIndex = 5;
+            this.button4.Text = ">>> BAĞLAN <<<";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 242);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(471, 164);
+            this.listBox1.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 412);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(470, 218);
+            this.textBox1.TabIndex = 7;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200",
+            "921600"});
+            this.comboBox2.Location = new System.Drawing.Point(11, 47);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(471, 28);
+            this.comboBox2.TabIndex = 8;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(11, 636);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(471, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 672);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Serial Communication/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Serial_Communication
+{
+    public partial class Form1 : Form
+    {
+        SerialPort sp = new SerialPort(); // Seri port nesnesi oluşturuyoruz
+        //SerialPort sp = new SerialPort("COM1", 9600, Parity.None, 8, StopBits.One);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            string[] portlar = SerialPort.GetPortNames(); // Bağlı seri portları diziye aktardık
+            foreach (string portAdi in portlar)
+            {
+                comboBox1.Items.Add(portAdi);
+            }
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 4;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+
+            try
+            {
+                if (!sp.IsOpen)
+                {
+                    if (comboBox1.SelectedItem != null && comboBox2.SelectedItem != null)
+                    {
+                        sp.PortName = comboBox1.SelectedItem.ToString(); // Kullanacağımız seri port adını seçiyoruz (String)
+                        sp.BaudRate = int.Parse(comboBox2.SelectedItem.ToString());  // Seri haberleşme hızını seçiyoruz (int32)
+                        sp.DataBits = 8; // göndereceğimiz bilginin kaç bitten oluşacağını bildiriyoruz (int32).
+                        sp.Parity = Parity.None; // Eşlik bitidir. Gönderilen verinin doğruluğunu kontrol etmek için kullanılır. 
+                        sp.StopBits = StopBits.One; // Stop bitinin kaç bit olacağını belirtir.
+                    }
+                    else
+                    {
+                        MessageBox.Show("Lütfen Bir Port Seçiniz...", "Hata");
+                    }
+
+                    sp.Open(); // Seri portumuzu açıyoruz
+                    MessageBox.Show("Bağlantı Başarılı...", "Bağlantı");
+                    button2.Enabled = true;
+                    button3.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("Bağlantı Zaten Açık...", "Bağlantı");
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Hata");
+            }
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sp.DataReceived += new SerialDataReceivedEventHandler(sp_DataReceived);
+            
+        }
+        int[] dizi = new int[100];
+        int i = 0;
+        private void sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+
+            int veri = sp.ReadByte();
+            dizi[i] = veri;
+            listBox1.Items.Add(veri.ToString());
+            textBox1.Text += veri.ToString()+",";
+            if (i==17)
+            {
+                i = -1;
+            }
+            i++;
+            if (dizi[1]<100)
+            {
+                progressBar1.Value = dizi[1];
+            }
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (sp.IsOpen)
+            {
+                sp.Close();
+                MessageBox.Show("Bağlantı Durduruldu...", "Bağlantı");
+                button2.Enabled = false;
+                button3.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Bağlantı Zaten Kapalı...", "Bağlantı");
+            }
+
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (sp.IsOpen)
+            {
+                sp.Close();
+            }
+        }
+    }
+}
